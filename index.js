c93e33be29cdedc366194d55d1091ed2112c06c0
Fabiola Paz <fabiihpaaz@gmail.com>
2022-08-11 00:34:22 -0300

    Criando try catch

---
+++ index.js
@@ -0,0 +1,9 @@
+try {
+   console.log(testando)
+    }
+    catch (error) {
+      console.error("Ops, isso não deveria acontecer, contate um desenvolvedor para que ele possa reparar esse erro!");
+      console.log("")
+      console.log(error)
+    }
+   
